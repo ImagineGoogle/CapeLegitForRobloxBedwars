d754afe2b920e2a146280ed0f4f2cdf22b0c4596
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2022-09-12 12:54:37 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -117,7 +117,7 @@ Main.ScrollBarThickness = 0
 Main.Visible = false
 
 UIListLayout_2.Parent = Main
-UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout_2.SortOrder = Enum.SortOrder.Name
 
 Label.Name = "Label"
 Label.Parent = Utility
@@ -271,6 +271,7 @@ end
 CreateModule("ClanDetector")
 CreateModule("SweatDetector")
 CreateModule("CustomAnimations")
+CreateModule("Cape")
 
 
 local function CheckForSweats()
@@ -319,6 +320,54 @@ for i, v in pairs(Main:GetChildren()) do
 					if isAlive() then lplr.Character.Humanoid.Jump = true end
 				elseif v.Text == "   SweatDetector" then
 					ScriptSettings.SweatDetector = true
+				elseif v.Text == "   Cape" then
+					local torso
+					if lplr.Character:FindFirstChild("Torso") then
+						torso = lplr.Character.Torso
+					else
+						torso = lplr.Character.UpperTorso
+					end
+					local p = Instance.new("Part", torso.Parent)
+					p.Name = "Cape"
+					p.Anchored = false
+					p.CanCollide = false
+					p.TopSurface = 0
+					p.BottomSurface = 0
+					p.Color = Color3.new(0, 0, 0)
+					p.Material = Enum.Material.Plastic
+					p.FormFactor = "Custom"
+					p.Size = Vector3.new(0.2,0.2,0.2)
+					local msh = Instance.new("BlockMesh", p)
+					msh.Scale = Vector3.new(9,17.5,0.5)
+					local motor = Instance.new("Motor", p)
+					motor.Part0 = p
+					motor.Part1 = torso
+					motor.MaxVelocity = 0.01
+					motor.C0 = CFrame.new(0,1.75,0) * CFrame.Angles(0,math.rad(90),0)
+					motor.C1 = CFrame.new(0,1,0.45) * CFrame.Angles(0,math.rad(90),0)
+					p.Color = Color3.fromRGB(40, 166, 255)
+					local wave = false
+					repeat task.wait(1/44)
+						local ang = 0.1
+						local oldmag = torso.Velocity.magnitude
+						local mv = 0.002
+						if wave then
+							ang = ang + ((torso.Velocity.magnitude/10) * 0.05) + 0.05
+							wave = false
+						else
+							wave = true
+						end
+						ang = ang + math.min(torso.Velocity.magnitude/11, 0.5)
+						motor.MaxVelocity = math.min((torso.Velocity.magnitude/111), 0.04) + mv
+						motor.DesiredAngle = -ang
+						if motor.CurrentAngle < -0.2 and motor.DesiredAngle > -0.2 then
+							motor.MaxVelocity = 0.04
+						end
+						repeat task.wait() until motor.CurrentAngle == motor.DesiredAngle or math.abs(torso.Velocity.magnitude - oldmag) >= (torso.Velocity.magnitude/10) + 1
+						if torso.Velocity.magnitude < 0.1 then
+							task.wait(0.1)
+						end
+					until not p or p.Parent ~= torso.Parent
 				end
 				SaveSettings()
 			else
@@ -330,6 +379,12 @@ for i, v in pairs(Main:GetChildren()) do
 					if isAlive() then lplr.Character.Humanoid.Jump = true end
 				elseif v.Text == "   SweatDetector" then
 					ScriptSettings.SweatDetector = false
+				elseif v.Text == "   Cape" then
+					if isAlive() then
+						pcall(function()
+							lplr.Character.Cape:Destroy()
+						end)
+					end
 				end
 				SaveSettings()
 			end
